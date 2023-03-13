bc6207a6baa89bf8f7b3f42500887ae1e6c942f6
marioRelajao <101522382+marioRelajao@users.noreply.github.com>
2023-03-13 18:38:02 +0100

    Added CRUD for Challenge
    Porfavor gihub dejame subirte desde el cliente que es desagradable nunca funcionas bien

--- src/controllers/challenge.ts
+++ src/controllers/challenge.ts
@@ -9,7 +9,7 @@ const get_Challenge =async ({params}:Request, res:Response) => {
         const data = response ? response:"NOT_FOUND";
         res.send(data);
     } catch(e){
-        handleHttp(res, "ERROR_GET_SUBJECT");
+        handleHttp(res, "ERROR_GET_CHALLENGE");
     }
 };
 
@@ -18,10 +18,47 @@ const get_Challenges =async (req:Request, res:Response) => {
         const response = await getChallenges();
         res.send(response);
     } catch(e){
-        handleHttp(res, "ERROR_GET_SUBJECT");
+        handleHttp(res, "ERROR_GET_Challenge");
     }
 };
 
-const update_Challenge =async ({params,body}:Request) => {
-    
-}
+const update_Challenge =async ({params,body}:Request, res:Response) => {
+    try{
+        const {idChallenge} = params;
+        const response = await updateChallenge(idChallenge, body);
+        res.send(response);
+    } catch (e){
+        handleHttp(res, "ERROR_UPDATE_CHALLENGE")
+    }
+};
+
+const post_Challenge = async ({body}:Request,res:Response)=>{
+    try{
+        const response = await insertChallenge(body);
+        res.send(response);
+    }catch(e){
+        handleHttp(res,"ERROR_POST_CHALLENGE");
+    }
+};
+
+const delete_Challenge = async ({params}:Request,res:Response)=>{
+    try{
+        const {idChallenge} = params;
+        const response = await deleteChallenge(idChallenge);
+        res.send(response);
+    } catch(e){
+        handleHttp(res,"ERROR_DELETE_CHALLENGE");
+    }
+};
+
+const accept_Challenge = async ({body}:Request,res:Response)=>{
+    try{
+        const { idUser, idChallenge } = body;
+        const response = await acceptChallenge(idUser, idChallenge);
+        res.send(response);
+    }catch(e){
+        handleHttp(res,"ERROR_ACCEPTING_CHALLENGE");
+    }
+};
+
+export{get_Challenge, get_Challenges, update_Challenge, delete_Challenge, accept_Challenge, post_Challenge};
