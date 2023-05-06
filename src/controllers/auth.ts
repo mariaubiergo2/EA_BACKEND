01beb95fca3f80a01cf3068f7c3abcf327ec36d4
Maria <maria.ubiergo@estudiantat.upc.edu>
2023-05-06 19:02:34 +0200

    robustness

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
 import { registerNewUser, loginUser, tokenUser } from "../services/auth";
+import { handleHttp } from "../utils/error.handle";
 
 const registerCtrl = async ({ body }: Request, res: Response) => {
-  const responseUser = await registerNewUser(body);
-  res.send(responseUser);
+  try{
+    const response = await registerNewUser(body);
+    if (response===("ALREADY_USER")){
+        res.status(400);
+        res.send(response)
+    }
+    else {
+        res.send(response);
+    }        
+}catch(e){
+    handleHttp(res, "ERROR_SIGNUP");
+}
+
 };
 
 const loginCtrl = async ({ body }: Request, res: Response) => {
@@ -28,7 +40,11 @@ const tokenCtrl = async ({ body }: Request, res: Response) => {
   if (responseUser === "PASSWORD_INCORRECT") {
     res.status(403);
     res.send(responseUser);
+  } else if (responseUser === "NOT_FOUND_USER") {
+    res.status(403);
+    res.send(responseUser);    
   } else {
+    res.status(200);
     res.send(responseUser);
   }
   
