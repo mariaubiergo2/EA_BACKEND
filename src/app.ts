0f3a1ad67b135753b92a84974ea11db34ba91c5e
Adrián <adquiros15@outlook.com>
2023-06-12 14:42:36 +0200

    chat seems working hehe

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import cors from "cors";
+import config from "config";
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 import "dotenv/config";
+import socket from "./socket";
 import db from "./config/mongo";
 import { router } from "./routes";
+import { version } from "../package.json";
 
 const PORT = process.env.PORT || 3001;  
 const app = express();
@@ -14,3 +19,27 @@ app.use(router);
 
 db().then(() => console.log("Connection is ready..."));
 app.listen(PORT, () => console.log(`Hey! Listening by the port ${PORT}.`));
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = "*"
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version ${version}`)
+);
+
+httpServer.listen(port, host, () => {
+  console.log("Server version ${version} is listening 🚀");
+  console.log(`http://${host}:${port}`);
+
+  socket({ io });
+});
