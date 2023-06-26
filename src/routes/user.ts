7c53f2054436839ea6fffb1465d689deb1e2c280
Maria <maria.ubiergo@estudiantat.upc.edu>
2023-06-26 12:48:03 +0200

    prova

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,7 +6,7 @@ import { getAllUsers, getUsers, getUser, getUserCount, getUsersProfile, getUserP
      addFollow, deleteFollow, addChallenge, disableUser, deleteUser, unableUser, getFollowing, 
      getNotFollowing, getFollowersCount, getNotFollowingCount, getFollowingCount, getFollowers, getHistory } from "../controllers/user";
 
-const router = Router();
+const router =  Router();
 
 //router.get("/get/all", checkAdmin, getAllUsers); //Get all users
 //router.get("/get/:idUser", checkAdmin, getUser); //Get only the information of one user
